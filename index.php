04213254a71b807d855714adb99ae7ac590952db
Yusuph <yusuphdarbo@gmail.com>
2025-11-26 16:41:17 +0000

    html for form section

--- index.php
+++ index.php
@@ -10,6 +10,39 @@
 
 <body>
 
+    <header>
+        <img class="logo" src="images/libraryLogo.png" alt="A open book">
+        <h2>Book Bank</h2>
+
+        <div class="registerSection">
+            <h4>Login</h4>
+            <h4>Register</h4>
+        </div>
+    </header>
+
+    <main>
+        <form>
+            <label for="Fname">First name</label>
+            <input type="text" id="Fname" name="Fname" placeholder="Bob" />
+
+            <label for="Lname">Last name</label>
+            <input type="text" id="Lname" name="Lname" placeholder="Smith" />
+
+            <label for="userName">Username</label>
+            <input type="text" id="userName" name="userName" placeholder="bobSmith123" />
+
+            <label for="Phone-number">Phone number</label>
+            <input type="number" id="phone-number" name="phone-number" placeholder="1234567891" />
+
+            <label for="password">password</label>
+            <input type="password" id="password" name="password" placeholder="******" />
+        </form>
+    </main>
+
+    <footer>
+
+    </footer>
+
 </body>
 
 </html>
