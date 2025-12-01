79c7236d07944e8c827386d2899679420fea2de4
Yusuph <yusuphdarbo@gmail.com>
2025-12-01 09:44:33 +0000

    added header and footer for register page

---
+++ register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Bank - Register</title>
+</head>
+
+<body>
+
+    <header>
+        <img class="logo" src="images/libraryLogo.png" alt="A open book">
+        <h2>Book Bank</h2>
+
+        <div class="registerSection">
+            <a href="index.php" class="nav-link active">Login</a>
+            <a href="#" class="nav-link">Register</a>
+        </div>
+    </header>
+
+    <footer>
+        <p>© 2025 Book Bank — All Rights Reserved</p>
+    </footer>
+
+</body>
+
+</html>
